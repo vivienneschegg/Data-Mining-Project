1c68397c1d2f12e3749162c0dc8690a66f4b9ca1
vivienneschegg <vivienne.schegg@bluewin.ch>
2026-03-07 14:49:51 +0100

    trying to collect the data in a data.frame

--- script/01_mining.R
+++ script/01_mining.R
@@ -11,7 +11,7 @@ firms_data <- data.frame(
   url = c(
           "https://www.depoly.co/about", "https://www.bloombiorenewables.com/company", 
           "https://www.divea.ch/", "https://www.cellx.ch/", "https://composite-recycling.ch/our-solution/", 
-          "https://grensolgroup.com/", "https://www.tethys-robotics.ch/de-ch/company/our-mission", 
+          "https://grensolgroup.com/", "https://www.tethys-robotics.ch/company/our-mission", 
           "https://oxyle.com/about", "https://www.comppair.ch/sustainability", "https://www.wattanywhere.com/about/", 
           "https://seprify.com/about", "https://www.exnaton.ai/en/about-us", "https://enerdrape.com/en/about-us/", 
           "https://viboo.io/en/uber-uns", "https://perovskia.solar/about-us/", 
@@ -25,13 +25,35 @@ firms_data <- data.frame(
     "Sustainability", "Environmental Sustainability", "Sustainability", "Sustainability", "Sustainability"
   ),
   
-  category = c("Recycling", "Biorenewables", "Water/Mission", "FoodTech", "Recycling", 
-               "Impact/Energy", "Robotics", "Water Treatment", "Material Science", "Energy", 
-               "Materials", "Energy/AI", "Energy", "Energy/Efficiency", "Solar Energy", 
-               "Energy", "Pharma", "Energy", "Food", "Food/Consumer Goods"),
-  
   stringsAsFactors = FALSE
 )
 
 head(firms_data)
+
+#Data.frame without the subpages
+firms2_data <- data.frame(
+  name = c("DePoly", "Bloom Biorenewables", "Divea", "CellX", "Composite Recycling", 
+           "Grensol", "Tethys Robotics", "Oxyle", "CompPair", "WattAnyWhere", 
+           "Seprify", "Exnaton", "Enerdrape", "viboo", "Perovskia Solar", 
+           "Alpiq", "Novartis", "Axpo", "Planted", "Nestlé"),
+  
+  type = c(rep("Startup", 15), rep("Incumbent", 5)),
+  
+  url = c(
+    "https://www.depoly.co/", "https://www.bloombiorenewables.com/", 
+    "https://www.divea.ch/", "https://www.cellx.ch/", "https://composite-recycling.ch/", 
+    "https://grensolgroup.com/", "https://www.tethys-robotics.ch/", 
+    "https://oxyle.com/", "https://www.comppair.ch/", "https://www.wattanywhere.com/", 
+    "https://seprify.com/", "https://www.exnaton.ai/", "https://enerdrape.com/", 
+    "https://viboo.io/", "https://perovskia.solar/", 
+    "https://www.alpiq.com/", "https://www.novartis.com/", 
+    "https://www.axpo.com/", "https://eatplanted.com/", 
+    "https://www.nestle.com/"),
+  
+  stringsAsFactors = FALSE
+)
+
+library(tidyverse)
+write_csv(firms2_data, "firms_data_starting.csv")
+
 
